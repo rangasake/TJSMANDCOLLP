a9e0f7154da9715389c09cf6e242ac01b969f0f3
ranga <rangaswamysake@gmail.com>
2021-11-23 16:40:56 +0530

    initial commit

---
+++ js/jsfile.js
@@ -0,0 +1,11 @@
+<script>
+$(function() {
+$(".startTyper").typed({
+strings: ["Hi, I'm Hitesh S Vikani, a software developer from Mumbai","This is CSS typing effect", "I think it's cool,isn't it?", "You can download files from github.com", "Have a good one!"],
+typeSpeed: 50, // typing speed
+backDelay: 100, // pause before backspacing
+loop: true, // loop on or off (true or false)
+loopCount: false, // number of loops, false = infinite
+});
+});
+</script>
